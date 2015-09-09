c56a43807765962a5d7b7b950783f98b2ff7cede
aereal <aereal@aereal.org>
2015-09-09 17:55:33 +0900

    minil new Ora

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Ora - It's new $module
+
+# SYNOPSIS
+
+    use Ora;
+
+# DESCRIPTION
+
+Ora is ...
+
+# LICENSE
+
+Copyright (C) aereal.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+aereal <aereal@aereal.org>
---
+++ lib/Ora.pm
@@ -0,0 +1,39 @@
+package Ora;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Ora - It's new $module
+
+=head1 SYNOPSIS
+
+    use Ora;
+
+=head1 DESCRIPTION
+
+Ora is ...
+
+=head1 LICENSE
+
+Copyright (C) aereal.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+aereal E<lt>aereal@aereal.orgE<gt>
+
+=cut
+
